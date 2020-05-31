592fda52caafe19483e49df043e3f86dbddd7bec
MrTomzor <tommymusil77@gmail.com>
2020-05-31 22:01:02 +0200

    added printing of test image

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("I am slightly inconvenienced by the SSH keys in github")
+import cv2
+from scipy.io import loadmat
+from scipy.misc import imsave
+
+data = loadmat('2020-04-23-10-15-56.mat')
+print(len(data['image_rgb'][0]))
+#res = cv2.connectedComponentsWithStats(data['image_rgb'])
+imsave('test_img.png', data['image_rgb'])
