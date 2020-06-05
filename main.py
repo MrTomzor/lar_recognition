8a85e85644296fd144e51f7046335eb154834935
MrTomzor <tommymusil77@gmail.com>
2020-06-05 18:28:05 +0200

    green saturation exception, contours working

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-import imageio
+#import imageio
 import numpy as np
 from scipy.io import loadmat
 from scipy.misc import imsave
@@ -8,7 +8,7 @@ data = loadmat('2020-04-23-10-15-56.mat')
 img = data['image_rgb']
 print(len(img))
 imsave('test_img.png', cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-
+  
 def getVectorsAngle():
     return 0
 
@@ -20,11 +20,12 @@ def getContours(color, img_hsv):
     height = len(img_hsv)
     print("img width", width, "img height", height)
 
-    mask = cv2.inRange(img_hsv, np.array([color - hueThreshold, 150, 50]), np.array([color + hueThreshold, 255, 255]))
+    mask = cv2.inRange(img_hsv, np.array([color - hueThreshold, 50 if (color > 40 and color < 60) else 150, 50]) , np.array([color + hueThreshold, 255, 255]))
     print(mask)
 
-    imageio.imwrite("mask.png", mask)
-    img_map, contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)    
+    #imageio.imwrite("mask.png", mask)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)    
+    imsave('test_mask.png',[[[x, x, x] for x in row] for row in mask]) 
 
 def getAllPoles():
     return 0
@@ -35,4 +36,4 @@ def getGoalParams():
 def solveProblem1():
     return 0
 
-getContours(0, img)
+getContours(50, cv2.cvtColor(img, cv2.COLOR_BGR2HSV))
