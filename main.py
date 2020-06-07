8a49854c3430a1c9c6800cc69d9152f79dd6dd80
MrTomzor <tommymusil77@gmail.com>
2020-06-07 12:26:09 +0200

    changed color ranges, can now accept data filename as argument

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 import cv2
+import sys
 #import imageio
 import numpy as np
 from scipy.io import loadmat
 from scipy.misc import imsave
 
-RED = 0
-GREEN = 55
-BLUE = 120 
+RED = 0 
+GREEN = 62
+BLUE = 95 
 COLOR_NAMES = {RED: 'RED', GREEN: 'GREEN', BLUE: 'BLUE'}
 POLE_RADIUS = 25
 ROBOT_WIDTH = 400
 SAFE_ROBOT_PASS_DISTANCE = 5
 
-#data = loadmat('2020-04-23-10-15-56.mat')
-data = loadmat('2020-04-23-10-17-17.mat')
-img = data['image_rgb']
-imsave('test_img.png', cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-K = data['K_rgb']
 
 def drawRect(img_hsv, x, y, w, h):
     clr = [0, 255, 255]
@@ -32,14 +28,11 @@ def getVectorsAngle(v1, v2):
     return np.arccos(np.dot(np.transpose(v1),v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
 
 def getContours(color, img_hsv):
-    hueThreshold = 10
-    saturationThreshold = 10
-    valueThreshold = 10
-
-    mask = cv2.inRange(img_hsv, np.array([color - hueThreshold, 50 if (color > 40 and color < 60) else 150, 50]) , np.array([color + hueThreshold, 255, 255]))
+    hueThreshold = 20
+    mask = cv2.inRange(img_hsv, np.array([color - hueThreshold, 70 if color == GREEN else 150, 50]) , np.array([color + hueThreshold, 255, 255]))
 
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)    
-    imsave('test_mask.png',[[[x, x, x] for x in row] for row in mask]) 
+    imsave('mask_' + COLOR_NAMES[color] + '.png',[[[x, x, x] for x in row] for row in mask]) 
     return contours
 
 def getPoles(colors, img_hsv, img_d, K):
@@ -51,8 +44,8 @@ def getPoles(colors, img_hsv, img_d, K):
             area = cv2.contourArea(cnt)
             lng = cv2.arcLength(cnt, True)
             x, y, w, h = cv2.boundingRect(cnt)
-            #print('AREA',  area)
-            if (area > 1000):
+            print('AREA',  area)
+            if (area > 400):
                 M = cv2.moments(cnt)
                 cx = int(round(M['m10'] / M['m00']))
                 cy = int(round(M['m01'] / M['m00']))
@@ -60,7 +53,7 @@ def getPoles(colors, img_hsv, img_d, K):
                 spaceVector = np.dot(invK, np.array([cx, cy, 1]))
                 spaceVector[1] = 0 # The y coordinate would only mess with distances
                 spaceVector = spaceVector * img_d[cy,cx]
-                if img_d[cy, cx] != 0:
+                if img_d[cy, cx]!= 0:
                     pole = {'color': color, 'col': cx, 'row': cy, 'depth': img_d[cy, cx], 'bound': [x, y, w, h], 'spaceVector': spaceVector}
                     print(pole)
                     poles.append(pole)
@@ -116,10 +109,18 @@ def solveProblem(data):
         params['color'] = color
         print('INFO: Nearest gate of color ' + COLOR_NAMES[color] + ': ',  params)
         gatesDetected.append(params)
+    if len(gatesDetected) == 0:
+        print('ERROR: Fewer than 2 poles found for each color, no gate detected')
+        exit(100)
     gatesDetected = sorted(gatesDetected, key=lambda x: x.get('v'), reverse = True)
     print('INFO: Nearest gate found :',  gatesDetected[0])
     return gatesDetected[0]
 
 
-#imsave('test_rect.png',drawRect(img, 30, 30, 100, 100))
-solveProblem(data)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('ERROR: No filename given! Please add the data filename as an argument while calling this script')
+        exit(101)
+    data = loadmat(sys.argv[1])
+    imsave('testimg.png',cv2.cvtColor(data['image_rgb'], cv2.COLOR_BGR2RGB))
+    solveProblem(data)
